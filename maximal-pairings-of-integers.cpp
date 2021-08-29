2dac9610d958fc4031024c1de397439ee01db6b5
jcgwt <86327210+jcgwt@users.noreply.github.com>
2021-08-29 23:00:26 +0100

    Adding C++ solution

---
+++ maximal-pairings-of-integers.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+bool CompareSecondElement(const std::pair<int,int>& a, const std::pair<int,int>& b);
+bool Loops(int a, int b);
+std::vector<std::pair<int, int>> CountOccurences(std::vector<int> ls);
+int CountMinimalLoops(std::vector<std::pair<int, int>> counts);
+
+int main()
+{
+  std::vector<int> input = {};
+  return CountMinimalLoops(CountOccurences(input));
+}
+
+bool CompareSecondElement(const std::pair<int,int>& a, const std::pair<int,int>& b)
+{
+  return a.second > b.second;
+}
+
+bool Loops(int a, int b)
+{
+  if (a * b == 0) return true;
+
+  int n = a + b;
+  while (n % 2 == 0) 
+    n = n / 2;
+
+  return ((a % n) * (b % n) != 0);
+}
+
+std::vector<std::pair<int, int>> CountOccurences(std::vector<int> ls)
+{
+  std::unordered_map<int, int> counts;
+  for (int& n : ls) 
+    counts[n]++;
+
+  std::vector<std::pair<int, int>> v_counts(counts.begin(), counts.end());
+  std::sort(v_counts.begin(), v_counts.end(), CompareSecondElement);
+  return v_counts;
+}
+
+int CountMinimalLoops(std::vector<std::pair<int, int>> counts)
+{
+  int imposs = 0;
+  auto ex_it = counts.begin();
+  while (ex_it != counts.end())
+  {   
+    auto in_it = ex_it;
+    int& x = ex_it -> second;
+    int& y =  in_it -> second;
+
+    while (in_it != counts.end())
+    {
+      if (Loops((*ex_it).first, (*in_it).first))
+      {
+        if (x > y)
+        {
+          x -= y;
+          y = 0;
+        }
+        else if (x <= y)
+        {
+          y -= x;
+          x = 0;
+          break;
+        }
+      }
+      
+    in_it = std::next(in_it, 1);
+    }
+
+  imposs += x;
+  ex_it = std::next(ex_it, 1);
+  }
+
+  return imposs;
+}
